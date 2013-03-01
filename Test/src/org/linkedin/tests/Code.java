e379b4327ea80d92c0d6c2a9e5f5ced0fd654482
Alek Slater <slayte@gmail.com>
2013-03-01 04:20:28 +0000

    added beginnings of a test project

---
+++ Test/src/org/linkedin/tests/Code.java
@@ -0,0 +1,17 @@
+package org.linkedin.tests;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class Code
+{
+	@Test
+	public void testPrintHelloWorld() 
+	{
+ 
+		Assert.assertEquals("Hello World", "Hello World");
+		
+	}
+}
